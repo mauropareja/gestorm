6106d60f09def4963750b73de97d8a0bd36b7c2a
Unknown <maurosped@gmail.com>
2019-02-13 17:46:39 -0500

    ready

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,17 +77,17 @@
             @endif
 
             <div class="content">
+            
+            
                 <div class="title m-b-md">
-                    Laravel
+                    Gestor de Mantenimiento
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Inicio</a>
+                    <a href="https://laracasts.com">Nosotros</a>
+                    <a href="https://laravel-news.com">Contacto</a>
+                    
                 </div>
             </div>
         </div>
